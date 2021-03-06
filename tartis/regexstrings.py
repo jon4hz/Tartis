7c3d071c95798ce059bf65eb324864efc4bd5670
jon4h <admin@psytux.com>
2021-03-06 23:35:26 +0100

    fix regex prefix

--- tartis/regexstrings.py
+++ tartis/regexstrings.py
@@ -2,10 +2,10 @@ import re
 
 __markets = ['USDT', 'BTC', 'ETH', 'BUSD', 'USD', 'BNB', 'TRX', 'XRP']
 
-REGEX_CRYPTO_PAIR = r"(\w+[\S\s]{0,3}?(?:" + '|'.join(__markets) + ")(?![\S\s]?(?:" + '|'.join(__markets) + ")))"
+REGEX_CRYPTO_PAIR = r"(\w+[\S\s]{0,3}?(?:" + '|'.join(__markets) + r")(?![\S\s]?(?:" + '|'.join(__markets) + r")))"
 # ex. (\w+[\S\s]{0,3}?(?:USDT|BTC|ETH|BUSD|USD|BNB|TRX|XRP)(?![\S\s]?(?:USDT|BTC|ETH|BUSD|USD|BNB|TRX|XRP)))
 
 REGEX_CRYPTO_PAIR_SPECIAL = r"([\W]|_)"
 
-REGEX_CRYPTO_PAIR_SLASH = r"([\w]+(" + '|'.join(__markets) + "))"
+REGEX_CRYPTO_PAIR_SLASH = r"([\w]+(" + '|'.join(__markets) + r"))"
 # ex. [\w]+(USDT|BTC|ETH|BUSD|USD|BNB|TRX|XRP)
