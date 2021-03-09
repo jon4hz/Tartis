271cbf43ca739ca24f5eb2ca7a1c820a76c1b72c
jon4h <admin@psytux.com>
2021-03-09 22:15:11 +0100

    testing

--- test/get_values_split_message.py
+++ test/get_values_split_message.py
@@ -1,9 +1,16 @@
 import split_message
-import re
+import re, os, sys
+
+# sys hacking for tartis import (parent folder)
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
+import tartis
 from texts import *
 
 def get_percent(text):
-    pass
+    print(re.findall(r"([\d])", j))
 
 for i in split_message.split_message(text3):
     for j in i:
