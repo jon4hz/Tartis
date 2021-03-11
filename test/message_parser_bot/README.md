980d7c442e574aa83b7c6de198fa3240bb74eb3b
jon4h <admin@psytux.com>
2021-03-11 01:24:13 +0100

    testing bot for parser

---
+++ test/message_parser_bot/README.md
@@ -0,0 +1,5 @@
+# HowTo
+
+## Softlink Tartis
+Create a softlink to access tartis as modul
+`ln -s ../../tartis/ .`
---
+++ test/message_parser_bot/bot.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3.8
+# 
+# Author: jon4hz
+# Date: 10.03.2021
+# Desc: Telegram Bot which validates signals and returns the output
+#
+#####################################################################################################################################################
+
+# import Bot Token
+from config import TOKEN
+# import python-telegram-bot modules
+from telegram.ext import (
+    Updater,
+    MessageHandler,
+    CommandHandler,
+    Defaults,
+    Filters
+)
+from telegram import (
+    ParseMode
+)
+# import tartis
+import tartis
+# other
+import sys
+
+#==================================================================================================
+# BOT FUNCTIONS
+#==================================================================================================
+
+def welcome(update, context):
+    context.bot.send_message(
+        chat_id = update.effective_chat.id,
+        text = 'Welcome! This is a live test for the message parser of <a href="github.com/jon4hz/tartis">tartis</a> \nJust send a Signal to the bot and it will try to interpret it as a trading signal.'
+    )
+
+def message_handler(update, context):
+    try:
+        signal = message_parser.parse_message(update.message.text)
+        context.bot.send_message(
+            chat_id = update.effective_chat.id,
+            text = signal
+        )
+        print(signal)
+    except Exception as e:
+        context.bot.send_message(
+            chat_id = update.effective_chat.id,
+            text = f'Error - {type(e).__name__} {e}'
+        )
+#==================================================================================================
+# MAIN
+#==================================================================================================
+
+# Create Tartis Message Parser
+message_parser = tartis.utils.messageParser()
+
+# Telegram
+defaults = Defaults(parse_mode=ParseMode.HTML)
+updater = Updater(TOKEN, use_context=True, defaults=defaults)
+dp = updater.dispatcher
+
+dp.add_handler(
+    CommandHandler('start', welcome)
+)
+
+dp.add_handler(
+    MessageHandler(Filters.text, message_handler)
+)
+
+updater.start_polling()
+updater.idle()
